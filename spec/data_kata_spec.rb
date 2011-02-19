23bc8214447ce4c52acc38397eca9a77a8589172
Pat Maddox <patmaddox@me.com>
2011-02-18 17:15:58 -0800

    wip kata

--- spec/data_kata_spec.rb
+++ spec/data_kata_spec.rb
@@ -1,91 +1,87 @@
-class WeatherReporter
-  def initialize(weather_data)
-    @weather_data = weather_data
-  end
-  
-  def print_max_temperature_difference
-    weather_report = @weather_data.sort_by {|d| d.temp_diff }.last
-    "#{weather_report.day} - #{weather_report.temp_diff}"
+# File.read("spec/weather.dat")
+
+class WeatherParser
+  attr_reader :records, :keys
+
+  def parse(data)
+    lines = data.split("\n")
+    have_seen_key_line = false
+    have_seen_finish_line = false
+
+    lines_to_parse = lines.select do |line|
+      have_seen_key_line ||= (line.split[0] == 'Dy')
+      have_seen_finish_line ||= (line.split[0] == 'mo')
+      have_seen_key_line && !have_seen_finish_line
+    end
+
+    @keys = lines_to_parse.shift.split
+    lines_to_parse.shift # remove empty line
+    # discard non-data line
+    @records = lines_to_parse.collect do |line|
+      values = line.split
+      @keys.zip(values).inject({}) {|hash, pair| hash[pair[0]] = pair[1]; hash }
+    end
   end
 end
 
 class WeatherData
-  attr_accessor :day, :min, :max
+  attr_reader :day, :temperature_difference
 
-  def initialize(day, max, min)
+  def initialize(day, high, low)
     @day = day
-    @max = max
-    @min = min
-  end
-  
-  def temp_diff
-    @max - @min
-  end
-  
-  def ==(other)
-    @day == other.day && @min == other.min && @max == other.max
+    @temperature_difference = high - low
   end
 end
 
-class WeatherDataParser
-  attr_reader :days
+describe WeatherData do
+  let(:data) { WeatherData.new 3, 90, 70 }
 
-  def initialize(data)
-    @data = data
-    parse
-  end
-  
-  def parse
-    @days = []
-    
-    in_data_section = false
-    @data.each_line do |line|
-      in_data_section = true if line.match(/Dy/)
-      in_data_section = false if line.match(/mo/)
-      
-      @days << parse_line(line.strip) if in_data_section && line.strip.match(/^\d/)
-    end
-  end
-  
-  def num_days
-    @days.size
+  it "knows its day" do
+    data.day.should == 3
   end
 
-  def on(day)
-    @days[day - 1]
-  end
-
-  def parse_line(line)
-    WeatherData.new(*line.split(/\s+/)[0..2].map {|d| d.to_i })
+  it "reports the temperature_difference" do
+    data.temperature_difference.should == 20
   end
 end
 
-describe "biggest temperature difference" do
-  it "shows the day with the largest temperature difference for a given month" do
-    weather_data = WeatherDataParser.new(File.read("spec/weather.dat")).days
-    WeatherReporter.new(weather_data).print_max_temperature_difference.should == "9 - 54"
+describe "parsing the weather file" do
+  it "returns a list of hashes with the data" do
+    parser = WeatherParser.new
+    parser.parse File.read("spec/weather_simple.dat")
+    parser.should have(2).records
+    parser.records[0]['Dy'].should == '1'
+    parser.records[0]['MxT'].should == '88'
+    parser.records[0]['MnT'].should == '59'
+    parser.records[1]['Dy'].should == '2'
+    parser.records[1]['MxT'].should == '79'
+    parser.records[1]['MnT'].should == '63'
+  end
+
+  it "knows the keys that were parsed" do
+    parser = WeatherParser.new
+    parser.parse File.read("spec/weather.dat")
+    parser.keys.should == ["Dy", "MxT", "MnT", "AvT", "HDDay", "AvDP", "1HrP", "TPcpn", "WxType", "PDir", "AvSp", "Dir", "MxS", "SkyC", "MxR", "MnR", "AvSLP"]
+  end
+
+  it "ignores junk lines" do
+    parser = WeatherParser.new
+    parser.parse File.read("spec/weather.dat")
+    parser.should have(30).records
+    parser.records[0]['Dy'].should == '1'
+    parser.records[0]['MxT'].should == '88'
+    parser.records[0]['MnT'].should == '59'
+    parser.records[29]['Dy'].should == '30'
+    parser.records[29]['MxT'].should == '90'
+    parser.records[29]['MnT'].should == '45'
   end
 end
 
-describe WeatherDataParser do
-  let(:parser) { WeatherDataParser.new(File.read("spec/weather.dat")) }
-
-  it "figures out the number of days in the data" do
-    parser.num_days.should == 30
+describe WeatherReporter do
+  it "prints out the day with the maximum temperature difference" do
+    parser = WeatherParser.new
+    parser.parse File.read("spec/weather_simple.dat")
+    reporter = WeatherReporter.new(parser.records)
+    reporter.report.should == "Day 9: 54"
   end
-
-  it "knows the maximum temperature for a given day" do
-    parser.on(1).should == WeatherData.new(1, 88, 59)
-    parser.on(1).max.should == 88
-    parser.on(30).max.should == 90
-  end
-end
-
-describe  WeatherData do
-  
-  it "should tell us the temperature difference for a given day" do
-    @weather_data = WeatherData.new(1, 88, 59)
-    @weather_data.temp_diff.should == 29
-  end  
-  
 end
